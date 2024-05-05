8ceaf00f23dfae7972c07b6a448043dab20eafd5
gilberto <gilbertowuwu0@gmail.com>
2024-05-05 09:42:27 -0400

    ...

--- static/create/main.js
+++ static/create/main.js
@@ -11,14 +11,19 @@ window.onload = async ()=>{
 	form_activities.onsubmit = async (e)=>{
 		e.preventDefault()
 		form_activities = document.querySelector('.form-activities')
-		const data = new FormData(form_activities)
+		const formData = new FormData(form_activities)
+		const data = Object.fromEntries(formData)
+		console.log(data)
 		//TODO: get how the fuck use formData
 		const csrf_token = await fetch('/get_csrf_token').then(data=>data.json())
-		const response = await fetch('/add_activity', {
+		let response
+		if(csrf_token){
+			response = await fetch('/add_activity', {
 			method: 'POST',
-			body: JSON.parse(data),
+			body: JSON.stringify(data),
 			headers: {'Content-type':'application/json', 'X-CSRF-Token':csrf_token.csrf_token}
-		})	
+			})	
+		}	
 		if(response.status == 200){
 			window.location.reload()
 		} else if(response.status == 400){
